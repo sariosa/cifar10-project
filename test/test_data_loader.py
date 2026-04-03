2f78b8a4b1de839923bdc8ef75b34664d04e50c8
sariosa <sara.ghahremany76@gmail.com>
2026-04-03 13:58:09 +0200

    Added test to see data loaded from data_loader.py

---
+++ test/test_data_loader.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, project_root)
+
+from src.data_loader import load_cifar10
+
+x_train, y_train, x_test, y_test, class_names = load_cifar10()
+
+print("Train shape:", x_train.shape)
+print("Test shape:", x_test.shape)
+print("Classes:", class_names)
